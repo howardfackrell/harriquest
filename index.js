412c6da5b3dcbf19702c373701b02a1eee4a6016
howardfackrell <howard.fackrell@.octanner.com>
2018-07-24 14:19:43 -0600

    small changes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log ('Hello Ashlyn')
+const express = require('express')
+
+const app = express()
+
+app.get('/:name([a-z]*[0-9])', function(req, response) {
+  let name = req.params['name']
+  response.send(`Hello ${name}`)
+})
+
+app.get('/harrison', function(req, response) {
+  response.send("Hello Harrison!!")
+})
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000")
+})
